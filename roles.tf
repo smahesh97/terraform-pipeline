5239ff6f563166f1dd4a8fe593aa1b283ee748fe
smahesh97 <smahesh.97@gmail.com>
2024-08-17 15:36:58 +0530

    Update roles.tf

--- roles.tf
+++ roles.tf
@@ -22,5 +22,5 @@ resource "aws_iam_role" "ecs_task_execution_role" {
 # ECS task execution role policy attachment
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
   role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
