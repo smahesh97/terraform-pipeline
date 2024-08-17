0ab554341fec0b6176db1a87f5057c885c17d9fd
smahesh97 <smahesh.97@gmail.com>
2024-08-17 14:17:01 +0530

    Update ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -3,12 +3,16 @@
 resource "aws_ecs_cluster" "main" {
   name = "cb-cluster"
 }
+data "aws_ecr_image" "service_image" {
+  repository_name = "int-demo"
+  image_tag       = "latest"
+}
 
 data "template_file" "cb_app" {
   template = file("./templates/ecs/cb_app.json.tpl")
 
   vars = {
-    app_image      = var.app_image
+    app_image      = data.aws_ecr_image.service_image.id
     app_port       = var.app_port
     fargate_cpu    = var.fargate_cpu
     fargate_memory = var.fargate_memory
