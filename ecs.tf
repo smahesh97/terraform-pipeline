f1c8caca479d56a977aec9da0bb858c2827b5582
smahesh97 <smahesh.97@gmail.com>
2024-08-18 12:39:25 +0530

    Update1 ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -15,7 +15,7 @@ data "template_file" "cb_app" {
   template = file("./templates/ecs/cb_app.json.tpl")
 
   vars = {
-    app_image      = data.aws_ecr_image.service_image.id
+    app_image      = data.aws_ecr_image.service_image.image_uri
     app_port       = var.app_port
     fargate_cpu    = var.fargate_cpu
     fargate_memory = var.fargate_memory
